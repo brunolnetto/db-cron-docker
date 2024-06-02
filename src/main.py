255914728868c80c274bd9e4279ae18967d1d55c
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-02 08:05:47 -0300

    feat/ remove folders required for download and extraction

--- src/main.py
+++ src/main.py
@@ -2,13 +2,10 @@
   - Nome do projeto : ETL - CNPJs da Receita Federal do Brasil
   - Objetivo        : Baixar, transformar e carregar dados da Receita Federal do Brasil
 """
-from database.schemas import Database
-from setup.base import get_sink_folder
+
 from setup.logging import logger
 from setup.config import get_database_uri
-
-# Folders and database setup
-download_folder, extract_folder = get_sink_folder()
+from database.schemas import Database
 
 # Get the database URI
 db_uri=get_database_uri()
