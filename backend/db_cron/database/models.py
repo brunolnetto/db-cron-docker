2ca7d95da8b444ffc11fa8c9cbb22cf272b0d89f
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-04 11:32:56 -0300

    feat/ remove uri build and new table name

--- backend/db_cron/database/models.py
+++ backend/db_cron/database/models.py
@@ -11,34 +11,19 @@ Base = declarative_base()
 
 T = TypeVar('T')
 
-class UserDBSchema(BaseModel, Generic[T]):
+class TokensDBSchema(BaseModel, Generic[T]):
     id: int = Field(..., description="Unique identifier for the user.")
-    username: str = Field(..., description="The username of the user.")
-    email: str = Field(..., description="The email of the user.")
-    password: str = Field(..., description="The password of the user.")
-    created_at: datetime = Field(..., description="The date and time the user was created.")
-    updated_at: datetime = Field(..., description="The date and time the user was last updated.")
+    token: str = Field(..., description="Token for the user.")
 
-class UserDB(Base):
+class TokensDB(Base):
     """
     SQLAlchemy model for the user table.
     """
-    __tablename__ = 'users'
+    __tablename__ = 'tokens'
     
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    username = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP, nullable=False)
-    updated_at = Column(TIMESTAMP, nullable=False)
+    token = Column(String, nullable=False)
 
     def __get_pydantic_core_schema__(self):
-        return UserDBSchema(
-            id=self.id,
-            username=self.username,
-            email=self.email,
-            password=self.password,
-            created_at=self.created_at,
-            updated_at=self.updated_at
-        )
+        return TokensDBSchema(id=self.id, token=self.token)
     
