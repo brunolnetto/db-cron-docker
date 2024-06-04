bdcf3ba3ec3d1ab1408e14543d130791cad03135
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-03 21:42:51 -0300

    config/ docker-compose and cron_task.sh scripts

--- scripts/cron_task.sh
+++ scripts/cron_task.sh
@@ -4,9 +4,6 @@
 # pip install -r /app/requirements.txt
 source "/opt/venv/bin/activate"
 
-pip3 list
-
 echo "Cron task is running" 
-echo "$(which python3)"
 /opt/venv/bin/python3 /app/backend/db_cron/main.py
 
